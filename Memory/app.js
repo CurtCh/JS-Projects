af8b6807b2c9a72500a5902e3c5abe99dcb08ae9
CurtCh <119529432+CurtCh@users.noreply.github.com>
2023-03-07 16:16:10 -0600

    Add files via upload

---
+++ Memory/app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+//card options
+const cardArray = [
+    {
+        name: 'blastoise',
+        img: 'images\blastoise_Art.png'
+    }
+    {
+        name: 'blastoise',
+        img: 'images\blastoise_Art.png'
+    }
+]
+})
