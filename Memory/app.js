f45ff0a831c862347d9605410a7173504ea35df0
CurtCh <119529432+CurtCh@users.noreply.github.com>
2023-03-08 08:21:32 -0600

    Add files via upload

--- Memory/app.js
+++ Memory/app.js
@@ -4,11 +4,118 @@ document.addEventListener('DOMContentLoaded', () => {
 const cardArray = [
     {
         name: 'blastoise',
-        img: 'images\blastoise_Art.png'
-    }
+        img: 'images/blastoise_Art.png'
+    },
     {
         name: 'blastoise',
-        img: 'images\blastoise_Art.png'
+        img: 'images/blastoise_Art.png'
+    },
+    {
+        name: 'charizard',
+        img: 'images/charizard_Art.png'
+    },
+    {
+        name: 'charizard',
+        img: 'images/charizard_Art.png'
+    },
+    {
+        name: 'mew',
+        img: 'images/mew_Art.png'
+    },
+    {
+        name: 'mew',
+        img: 'images/mew_Art.png'
+    },
+    {
+        name: 'mewtwo',
+        img: 'images/mewtwo_Art.png'
+    },
+    {
+        name: 'mewtwo',
+        img: 'images/mewtwo_Art.png'
+    },
+    {
+        name: 'pikachu',
+        img: 'images/pikachu_Art.png'
+    },
+    {
+        name: 'pikachu',
+        img: 'images/pikachu_Art.png'
+    },
+    {
+        name: 'venusaur',
+        img: 'images/venusaur_Art.png'
+    },
+    {
+        name: 'venusaur',
+        img: 'images/venusaur_Art.png'
     }
 ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+
+const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+var cardsChosen = []
+var cardsChosenID = []
+var cardsWon = [] 
+
+//create your board
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/card_Art.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        grid.appendChild(card)
+    }
+}
+
+// check for matches
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneID = cardsChosenID[0]
+    const optionTwoID = cardsChosenID[1]
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match!!!')
+        cards[optionOneID].setAttribute('src', 'images/empty_Art.png')
+        cards[optionTwoID].setAttribute('src', 'images/empty_Art.png')
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneID].setAttribute('src', 'images/card_Art.png')
+        cards[optionTwoID].setAttribute('src', 'images/card_Art.png')
+        alert('Sorry, try again!')
+    }
+    cardsChosen = []
+    cardsChosenID = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You found them all!'
+    }
+}
+
+//filp your card
+function flipCard() {
+    var cardID = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardID].name)
+    cardsChosenID.push(cardID)
+    this.setAttribute('src', cardArray[cardID].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+function checkDone() {
+    if (cards[optionOneID].setAttribute = 'images/empty_Art.png') {
+        alert('Match already found. Please select a new card.')
+    } else {
+        pass
+    }
+}
+
+
+createBoard()
+
+
 })
